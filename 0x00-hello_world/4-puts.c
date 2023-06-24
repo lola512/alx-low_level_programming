7380f923adcda4cc7d13bc578bcdd3cc45653ea4
lola512 <wtaha512@gmail.com>
2023-06-24 00:43:59 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - program print with puts fumction
+ * return: 0 (success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
