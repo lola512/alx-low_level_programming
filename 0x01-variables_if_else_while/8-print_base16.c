09f4ba953b28714961fb03e16876117f11317fbb
lola512 <wtaha512@gmail.com>
2023-06-30 14:37:42 -0700

    base16_1

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - print 16 numbers
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 11; i++)
+	{
+		putchar(i);
+	}
+	for (i = 0; i < 7; i++)
+	{
+		putchar(i)
+	}
+	putchar('\n');
+	return (0);
+}
