f29c5881ba4db1116b6e085020ad8c72d2b284d9
lola512 <wtaha512@gmail.com>
2023-06-30 16:55:53 -0700

    comp5_1

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible combinations of two two-digit numbers
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int i, l;
+	for (i = 0; i < 100; i++)
+	{
+		for (l = 0; l < 100; l++)
+		{
+			if (i > l)
+			{
+				putchar((i / 10) + 48);
+				putchar((i % 10) + 48);
+				putchar(' ');
+				putchar((l / 10) + 48);
+				putchar((l % 10) + 48);
+				if (i != 98 || l != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
