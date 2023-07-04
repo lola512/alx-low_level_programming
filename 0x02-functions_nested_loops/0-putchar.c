3519b56298a746552a2df4a1690463f0fc9a09af
lola512 <wtaha512@gmail.com>
2023-07-04 16:14:42 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+
+#include <main.h>
+/**
+ * main - print_putchar
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+        _putchar("_putchar");
+        _putchar("\n");
+        return (0);
+}
