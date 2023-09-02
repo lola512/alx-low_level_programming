63ceed25cb7b32659140a44755c89b9b335a6d57
walaa <wtaha512@gmail.com>
2023-09-02 04:27:36 -0700

    fizzbuzz2

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,28 +10,27 @@
 int main(void)
 {
 	int i;
-	
+
 	for (i = 1; i <= 100; i++)
 	{
 		if (i % 3 == 0 && i % 5 != 0)
 		{
 			printf(" Fizz");
-
 		} else if (i % 5 == 0 && i % 3 != 0)
 		{
 			printf(" Buzz");
-		} else if ( i % 3 == 0 && i % 5 == 0)
+		} else if (i % 3 == 0 && i % 5 == 0)
 		{
 			printf(" FizzBuzz");
 		} else if (i == 1)
 		{
-			printf ("%d", i);
-		} else 
+			printf("%d", i);
+		} else
 		{
-			printf ("%d"; i);
+			printf(" %d", i);
 		}
 	}
-	printf ("\n");
+	printf("\n");
 
 	return (0);
 }
